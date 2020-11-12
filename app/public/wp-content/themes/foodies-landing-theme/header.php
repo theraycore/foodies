e17bcd1c9f9632dfb9dc71ad84ded729f93a7d61
theraycore <raymundo.host@gmail.com>
2020-11-12 14:56:49 -0600

    Navbar and Hero OK

--- app/public/wp-content/themes/foodies-landing-theme/header.php
+++ app/public/wp-content/themes/foodies-landing-theme/header.php
@@ -8,3 +8,60 @@
   <?php wp_head(); ?>
 </head>
 <body>
+
+<!-- NavBar -->
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item navbar-logo" href="https://bulma.io">
+      Foodies
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="main-navbar" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Acerca de
+      </a>
+
+      <a class="navbar-item">
+        Restaurantes
+      </a>
+
+      <a class="navbar-item">
+        Menú
+      </a>
+
+      <a class="navbar-item">
+        Contáctanos
+      </a>
+    </div>
+  </div>
+</nav>
+<!-- /NavBar -->
+
+<!-- Hero -->
+<section class="hero">
+  <div class="hero-body columns">
+    <div class="container column">
+      <h1 class="title">
+        Un nuevo sabor esta en la ciudad
+      </h1>      
+      <p>Estamos a punto de descubrir un mundo lleno de sabores y de emociones inigualables.</p>
+      <br>
+      <a href="" class='hero-btn'>Encuentranos -></a>
+    </div>
+
+    <div class="side-image column">
+      <figure class="image">
+        
+      </figure>
+    </div>
+  </div>
+</section>
+<!-- /Hero -->
