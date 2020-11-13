1f9867689543b672116947ebaf6ebb87f1e506e2
theraycore <raymundo.host@gmail.com>
2020-11-13 12:17:18 -0600

    Testimonials OK

--- app/public/wp-content/themes/foodies-landing-theme/front-page.php
+++ app/public/wp-content/themes/foodies-landing-theme/front-page.php
@@ -100,4 +100,19 @@
 </div>
 <!-- /Restaurants -->
 
+<!-- Feedbacks -->
+<div class="section-feedbacks">
+  <figure class="ketchup-img"></figure>
+  <h1 class="feed-title">“El mejor lugar para degustar en familia y amigos!”</h1>
+  <p class="feed-paragraph">Es el mejor lugar al que he venido con mi familia, la comida es rica, sirven rápido y te atienden de la mejor manera. </p>
+
+  <div class="flow-control">
+    <button class="btn-left"></button>
+    <label>1 / 5</label>
+    <button class="btn-right"></button>
+  </div>
+</div>
+<!-- /Feedbacks -->
+
+
 <?php get_footer(); ?>
