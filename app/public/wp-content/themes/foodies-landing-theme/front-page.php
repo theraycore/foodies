00cd29bc16318d9aa64075117ada9360dec167ee
theraycore <raymundo.host@gmail.com>
2020-11-13 22:11:36 -0600

    Mobile App OK

--- app/public/wp-content/themes/foodies-landing-theme/front-page.php
+++ app/public/wp-content/themes/foodies-landing-theme/front-page.php
@@ -159,4 +159,62 @@
 </div>
 <!-- /Contact Us -->
 
+<!-- Mobile App -->
+<div class="section-mobile-app">
+
+  
+
+  <!-- Columns -->
+  <div class="columns">
+    <div class="column left-image">     
+      <!-- Phone Image Anchor -->
+      <div class="phone-image-anchor">    
+      </div> 
+    </div>
+    <div class="column right-content is-7">
+      <div class="features-anchor">
+        <div class="features-content">
+          <h1 class="title">
+            Obten más beneficios <br> Descarga nuestra App
+          </h1>
+
+          <div class="features-numbers">
+            <div class="columns">
+              <!-- 1 -->
+              <div class="column">
+                <div class='feature-item'>
+                  <div class="circle-number" id="circle-number-01">01</div>
+                  <h2 class="title">Solicita rápido</h2>
+                  <p>Curabitur in eleifend turpis, id vehicula odio. Donec pulvinar tellus eget magna aliquet ultricies.</p>
+                </div>
+              </div>
+              <!-- 2 -->
+              <div class="column">
+                <div class='feature-item'>
+                  <div class="circle-number" id="circle-number-02">02</div>
+                  <h2 class="title">Fácil de Usar</h2>
+                  <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+              </div>
+            </div>
+            <div class="columns">            
+              <!-- 3 -->
+              <div class="column">
+                <div class='feature-item' id="feature-item-03">
+                  <div class="circle-number">03</div>
+                  <h2 class="title">Promociones especiales</h2>
+                  <p>Curabitur in eleifend turpis, id vehicula odio. Donec pulvinar tellus eget magna aliquet ultricies.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /Mobile App -->
+
 <?php get_footer(); ?>
